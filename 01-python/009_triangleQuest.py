529761fbe2e911e8d2feeb5b52e5beeb1bf4679d
Alan Nguyen <tuan8101@gmail.com>
2019-07-25 10:03:00 -0600

    Add test cases

--- 01-python/009_triangleQuest.py
+++ 01-python/009_triangleQuest.py
@@ -2,8 +2,11 @@ def triangleQuest(n):
     """ Print a numerical triangle of height n - 1,
      using only arithmetic operations and a single for loop
      """
-    for i in range(1,int(input())):
+    for i in range(1, n):
         print((10**(i)//9)*i)
 
 
 # test case
+triangleQuest(5)
+
+triangleQuest(10)
