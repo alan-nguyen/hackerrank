80f3f8c5e4d851e05020f3b0e477a3bca377255c
Alan Nguyen <tuan8101@gmail.com>
2019-07-25 10:01:30 -0600

    Create Triangle quest

---
+++ 01-python/009_triangleQuest.py
@@ -0,0 +1,9 @@
+def triangleQuest(n):
+    """ Print a numerical triangle of height n - 1,
+     using only arithmetic operations and a single for loop
+     """
+    for i in range(1,int(input())):
+        print((10**(i)//9)*i)
+
+
+# test case
