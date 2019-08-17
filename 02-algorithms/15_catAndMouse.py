984db03eeaf13a3a3b7c44f726d3b0e990859699
Alan Nguyen <tuan8101@gmail.com>
2019-08-17 15:28:36 +0700

    Create catAndMouse function

---
+++ 02-algorithms/15_catAndMouse.py
@@ -0,0 +1,10 @@
+def catAndMouse(x, y, z):
+    catA = abs(x - z)
+    catB = abs(y - z)
+    if catA > catB:
+        return 'Cat B'
+    elif catA < catB:
+        return 'Cat A'
+    else:
+        return'Mouse C'
+
